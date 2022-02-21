11a1be43f910ad788dfebc1056432408fbbdd483
dollarstir <kpin463@gmail.com>
2022-02-21 08:12:29 +0000

    handle error thrown when user refuse to pass array

--- test.php
+++ test.php
@@ -1,50 +1,60 @@
 <?php
-$color = 1;
-if (isset($context['color'])) {
-    $color = intval($context['color']);
-}
+el::doctype();
+el::html('lang="en"', 
+[
+    el::head('',
+    [
+        meta::charset('charset="utf-8"'),
+        meta::browser('http-equiv="X-UA-Compatible" content="IE=edge"'),
+        meta::viewport('name="viewport" content="width=device-width, initial-scale=1.0"'),
+        el::title('Yolk Framework'),
+        Yolk::uicore('allcss')
+    ]
+    ),
+    el::body('',
+    [
+        el::img(Path::rebase('j/after.png'),'width=100 height=100"'),
+        el::h3('class="text-center m-4 text-success"', ['Thanks for choosing the Yolk framework!!!']),
+        el::h4('class="text-muted m-4"',[
+
+        
+            el::ul('',
+            [
+                el::li('style="width: fit-content"',
+                [
+                    el::span('', ['framework']),
+                    el::span('',[ $context['framework']])
+                ]
+                ),
+                el::li('style="width: fit-content"',
+                [
+                    el::span('', ['color']),
+                    el::span('', [$context['color']])
+                ]
+                ),
+               
+                
+            ]
+            )
+        ]
+        ),
+        el::h4('class="text-center text-muted mt-5"',
+        [
+            el::a('https://way2tutorial.com/html/tag/index.php','',
+            
+               'Click here to login'
+            
+          )
+        ]
+        )
+    ]
+    )
+]);
+
+
+
+
+
+
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Document</title>
-
-    <?php Yolk::uicore('allcss'); ?>
-</head>
-
-<body>
-    <img width=100 height=100 src="<?= Path::rebase('j/after.png'); ?>">
-
-    <h3 class="text-center m-4 <?= ($color == 1 ? 'text-success' : 'text-danger'); ?>">
-        <i class="fa-solid fa-fire"></i>
-        Thanks for choosing the Yolk framework!!!
-    </h3>
-
-    <h4 class="text-muted m-4">
-        <ul>
-            <?php
-            foreach ($context as $key => $value) {
-                echo '
-                    <li style="width: fit-content"><span>'.$key.'</span> : <span>'.$value.'</span></li>
-                ';
-            }
-            ?>
-        </ul>
-    </h4>
-
-    <h4 class="text-center text-muted mt-5">
-        Try out this link :
-        <a href="<?= Path::rebase('test/york/'.(++$color % 2)); ?>">
-            <i class="fa fa-face-laugh-wink <?= ($color == 1 ? 'text-danger' : 'text-success'); ?>"></i>
-        </a>
-    </h4>
-
-</body>
-
-</html>
