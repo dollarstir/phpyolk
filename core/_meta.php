8c0a4e955549c23a808b30b6d4acb78745dc5478
dollarstir <kpin463@gmail.com>
2022-02-21 21:03:59 +0000

    changed meta

--- core/_meta.php
+++ core/_meta.php
@@ -2,22 +2,22 @@
 class meta{
     
     public static function description($description=''){
-        echo '<meta name="description" content="'.$description.'">';
+        return '<meta name="description" content="'.$description.'">';
     }
     public static function keywords($keywords=''){
-        echo '<meta name="keywords" content="'.$keywords.'">';
+        return '<meta name="keywords" content="'.$keywords.'">';
     }
     public static function author($author=''){
-        echo '<meta name="author" content="'.$author.'">';
+        return '<meta name="author" content="'.$author.'">';
     }
     public static function charset($charset='UTF-8'){
-        echo '<meta charset="'.$charset.'">';
+        return '<meta charset="'.$charset.'">';
     }
     public static function viewport($viewport='width=device-width, initial-scale=1.0'){
-        echo '<meta name="viewport" content="'.$viewport.'">';
+        return '<meta name="viewport" content="'.$viewport.'">';
     }
     public static function browser($eq ='X-UA-Compatible',$browser='IE=edge'){
-        echo '<meta http-equiv="'.$eq.'" content="'.$browser.'">';
+        return '<meta http-equiv="'.$eq.'" content="'.$browser.'">';
     }
   
 }
