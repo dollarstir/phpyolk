290e89e92a2455444ea802569e5e5b69a4c3f73b
aframson <obiriaframson@gmail.com>
2022-02-24 14:38:36 +0000

    layouts

--- test.php
+++ test.php
@@ -1,57 +1,84 @@
 <?php
-  
+
 // Developed by Frederick Ennin (Dollarstir)
 
-YolkUI::run( new Wrapper(
-    el::html('lang="en"', 
-[
-    el::head('',
-    [
-        meta::charset(),
-        meta::browser(),
-        meta::viewport(),
-        el::title('Yolk Framework'),
-        Yolk::uicore('allcss'),
-
-       el::linkcss('style.css'),
-    ]
-    ),
-    el::body('',
-    [
-        Container::fluid('',
+YolkUI::run(new Wrapper(
+    el::html(
+        'lang="en"',
         [
-            el::divi('style="margin-top: 0px"'),
-            Layout::row('',
-            [
-                
-                Layout::colm('12','',
+            el::head(
+                '',
                 [
-                    Navbar::basiclight('style="height:50px;"',navbar::Navbrand('home',[el::h2('class="kofi"',['Php Yolk'])]),
-                    [
-                        Navbar::Navactive('',['Home' =>'home']),
-                        Navbar::Navbasic('',['Services'=>'services/sjdsdjs']),
-                        Navbar::Navbasic('',['Documentation'=>'documentation']),
-                        Navbar::Navdropdown('','Pages',['Contact Us'=>"contact",'About Us' =>'about',]),
-                    ]),
-                ]),
-            ]),
-            Layout::row('',
-            [
-                container::basic('id="maindiv"',
+                    meta::charset(),
+                    meta::browser(),
+                    meta::viewport(),
+                    el::title('Yolk Framework'),
+                    Yolk::uicore('allcss'),
+                    el::linkcss('style.css'),
+                ]
+            ),
+            el::body(
+                '',
                 [
-                    el::divi('id="navlist"',
-                    [
-                        el::input('search','search','inps','','placeholder="Search..."')
-                    ]),
-                    el::divi('id="contents"')
-                ]),
-             
-            ]),
-       ]),
-        Yolk::uicore('alljs'),
-    ]
-    ),
-])));
+                    Container::fluid(
+                        '',
+                        [
+                            el::divi('style="margin-top: 0px"'),
+                            Layout::row(
+                                '',
+                                [
 
+                                    Layout::colm(
+                                        '12',
+                                        '',
+                                        [
+                                            Navbar::basiclight(
+                                                'style="height:50px;"',
+                                                navbar::Navbrand('home', [el::h2('class="kofi"', ['Php Yolk'])]),
+                                                [
+                                                    Navbar::Navactive('', ['Home' => 'home']),
+                                                    Navbar::Navbasic('', ['Services' => 'services/sjdsdjs']),
+                                                    Navbar::Navbasic('', ['Documentation' => 'documentation']),
+                                                    Navbar::Navdropdown('', 'Pages', ['Contact Us' => "contact", 'About Us' => 'about',]),
+                                                ]
+                                            ),
+                                        ]
+                                    ),
+                                ]
+                            ),
+                            Layout::row(
+                                '',
+                                [
+                                    container::basic(
+                                        'id="maindiv"',
+                                        [
+                                            el::divi(
+                                                'id="navlist"',
+                                                [
+                                                    el::divi('id="searchbox"', [
+                                                        icons::far('search','15','','se'),
+                                                        el::input('search', 'search', 'inps', '', 'placeholder="Search..."'),
+                                                    ]),
+                                                    el::divi('', [
+                                                        el::h4('id="doctitle"', 'Documentation'),
+                                                    ])
+                                                ]
+                                            ),
+                                            el::divi('id="contents"',[
+                                                el::divi('id="getStarted"',[
+                                                    'Get Started',
+                                                ])
+                                            ])
+                                        ]
+                                    ),
 
-
+                                ]
+                            ),
+                        ]
+                    ),
+                    Yolk::uicore('alljs'),
+                ]
+            ),
+        ]
+    )
+));
