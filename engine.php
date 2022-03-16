62a59d5b73891ec556804793e38aae61c51e1fcb
dollarstir <kpin463@gmail.com>
2022-03-16 21:08:36 +0000

    dfsd

--- engine.php
+++ engine.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'loader/autoloader.php';
-
 $router = new Router([
     new Route(
         '/',
@@ -10,49 +9,10 @@ $router = new Router([
         }
     ),
     new Route(
-        '/test2',
-        function ($context) {
-            return Viewer::view('/test2.php', $context);
-        }
-    ),
-    new Route(
-        '/test',
-        function ($context) {
-            return Viewer::view('/test.php', $context);
-        }
-    ),
-    new Route(
-        '/documentation',
-        function ($context) {
-            return Viewer::view('/docs.php', $context);
-        }
-    ),
-    new Route(
-        '/t4',
-        function ($context) {
-            return Viewer::view('/test4.php', $context);
-        }
-    ),
-    new Route(
-        '/t5',
-        function ($context) {
-            return Viewer::view('/test5.php', $context);
-        }
-    ),
-
-    new Route(
-        '/t3',
-        function ($context) {
-            return Viewer::view('/test3.php', $context);
-        }
-    ),
-
-    new Route(
         '/login1',
         function ($context) {
             return Viewer::view('/widgets/login/login.php', $context);
         }
     ),
 ]);
-
 $router->launch();
