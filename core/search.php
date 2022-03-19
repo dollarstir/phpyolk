76decb031cc222bf8b0998b1bfa19668285f65e9
dollarstir <kpin463@gmail.com>
2022-03-19 10:30:09 +0000

    fdfd

--- core/search.php
+++ core/search.php
@@ -11,7 +11,7 @@ class search extends database{
                     $vs .='WHERE '.$value.' LIKE :'.$value;
                 }
                 else{
-                    $vs .='OR '.$value.' LIKE :keyword';
+                    $vs .=' OR '.$value.' LIKE :'.$value;
                 }
 
           
@@ -28,7 +28,7 @@ class search extends database{
                 $ord = 'ORDER BY '.$key.' '.$value;
             }
         }
-        // var_dump($vs);
+        var_dump($vs);
         $sq = $this->conn->prepare("SELECT * FROM $table $vs $ord $l");
 
         foreach($record as $value){
