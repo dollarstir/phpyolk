03fc63f4a2f9e11d65a0e22206d17f9af2ef04b7
dollarstir <kpin463@gmail.com>
2022-03-19 07:47:28 +0000

    dsfsdf

--- core/search.php
+++ core/search.php
@@ -4,6 +4,12 @@ class search{
     public function basic($table,$record,$order = '',$by='',$limit =''){
         $vs = '';
 
+        foreach ($record as $value){
+            if(is_array($value)){
+                
+            }
+        }
+
 
 
 
