c1bb35444e4494a3781f94015bb3a59a333d71d4
dollarstir <kpin463@gmail.com>
2022-03-19 07:53:48 +0000

    dsfdsfs

--- core/search.php
+++ core/search.php
@@ -1,12 +1,13 @@
 <?php
 class search{
 
-    public function basic($table,$record,$order = '',$by='',$limit =''){
+    public function basic($table,$searchword,$record,$order = '',$by='',$limit =''){
         $vs = '';
 
         foreach ($record as $value){
             if(is_array($value)){
-                
+                vs .='WHERE '.$value.' LIKE '
+
             }
         }
 
