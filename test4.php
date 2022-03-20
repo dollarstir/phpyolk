579f8c5af3e0ae62600e357dda91a8c08e36017d
dollarstir <kpin463@gmail.com>
2022-03-20 12:23:56 +0000

    dasdasdas

--- test4.php
+++ test4.php
@@ -7,7 +7,6 @@ YolkUI::run(new Wrapper(
         'lang="en"',
         [
             el::head(
-                
                 [
                     meta::charset(),
                     meta::browser(),
@@ -15,21 +14,24 @@ YolkUI::run(new Wrapper(
                     el::title('Yolk Framework - SSR'),
                     Yolk::uicore('corecss'),
                    import('css'),
-                    
+                   el::style('', [
+                       'body{
+                           '.backgroundcolor('#116699', true).'
+                       }',
+                   ]),
                 ]
             ),
             el::body(
                 [
-                    el::a('t5','', 'Testing SSR functionality!'),
+                    el::a('t5', '', 'Testing SSR functionality!'),
                     // el::br(),
 
-                    el::div('class="container"',[
-                        Yolk::input('text','','search','search'),
-                        el::div('id="display"',[
-                            
+                    el::div('class="container"', [
+                        Yolk::input('text', '', 'search', 'search'),
+                        el::div('id="display"', [
                         ]),
                     ]),
-                    Yolk::uicore('corejs')
+                    Yolk::uicore('corejs'),
                 ]
             ),
         ]
