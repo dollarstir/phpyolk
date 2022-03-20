e6f7a9485c198813c3dac2f933f33af36d0c7212
dollarstir <kpin463@gmail.com>
2022-03-20 13:04:51 +0000

    sdsdf

--- core/css.php
+++ core/css.php
@@ -14,3 +14,33 @@
 
          return trim($tag);
      }
+
+     function dot($classname = '', $content = [])
+     {
+         if (is_array($content)) {
+         } else {
+             $content = [$content];
+         }
+         $tag = '.'.$classname.'{';
+         foreach ($content as $key) {
+             $tag .= $key;
+         }
+         $tag .= '}';
+
+         return trim($tag);
+     }
+
+     function widget($widgetname = '', $content = [])
+     {
+         if (is_array($content)) {
+         } else {
+             $content = [$content];
+         }
+         $tag = $widgetname.'{';
+         foreach ($content as $key) {
+             $tag .= $key;
+         }
+         $tag .= '}';
+
+         return trim($tag);
+     }
