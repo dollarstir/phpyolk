fcf4be0aa2919c223827c630e063a700bd1253ab
dollarstir <kpin463@gmail.com>
2022-03-24 14:20:27 +0000

    asdsa

--- core/yc.php
+++ core/yc.php
@@ -1774,12 +1774,12 @@ function alignitems($value = '', $isimportant = false)
  }
  
 
- function all($value = '', $isimportant = false)
+ function paddingtop($value = '', $isimportant = false)
  {
      if ($isimportant) {
-         return ':'.$value.' !important;';
+         return 'padding-top:'.$value.' !important;';
      } else {
-         return ':'.$value.';';
+         return 'padding-top:'.$value.';';
      }
  }
  
