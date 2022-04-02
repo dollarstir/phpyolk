dee5e74ef8af5f6e0ffa06a16ba02a8e6e8f79ce
dollarstir <kpin463@gmail.com>
2022-04-02 10:15:39 +0000

    dsfsdf

--- core/sel.php
+++ core/sel.php
@@ -37,9 +37,9 @@ class Sel extends database
         // $vs = rtrim($vs, $conjunction);
         // echo $vs;
         if ($limit == '') {
-            $sel = $this->conn->prepare("SELECT * FROM $table $vs");
+            $sel = $this->conn->prepare("SELECT * FROM $table $vs $kof ");
         } else {
-            $sel = $this->conn->prepare("SELECT * FROM $table $vs $limit");
+            $sel = $this->conn->prepare("SELECT * FROM $table $vs $kof LIMIT $limit");
         }
         foreach ($target as $value) {
             if (is_array($value)) {
