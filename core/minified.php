e8d2ac2bdf6c85b01c118e329669bfc703d35222
dollarstir <kpin463@gmail.com>
2022-04-02 09:51:19 +0000

    ddfsf

--- core/minified.php
+++ core/minified.php
@@ -8,6 +8,6 @@ function fetchall($table, $order = [], $limit = '')
     return $response;
 }
 
-function customfetch(){
-    
+function customfetch($table, $target, $conjunction = '', $limit = '')
+{
 }
