2db6928eba2042c3f7630798ceb9410aeb527c25
dollarstir <kpin463@gmail.com>
2022-04-12 12:27:46 +0000

    eewfefw

--- engine.php
+++ engine.php
@@ -5,28 +5,7 @@ $router = new Router([
     new Route(
         '/',
         function ($context) {
-            return Viewer::view('main.php', $context);
-        }
-    ),
-
-    new Route(
-        '/check',
-        function ($context) {
-            return Viewer::view('test.php', $context);
-        }
-    ),
-
-    new Route(
-        '/check/{rid}',
-        function ($context) {
-            return Viewer::view('test.php', $context);
-        }
-    ),
-
-    new Route(
-        '/t/{rid}',
-        function ($context) {
-            return Viewer::view('t/test.php', $context);
+            return Viewer::view('install.php', $context);
         }
     ),
 ]);
